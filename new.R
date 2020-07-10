5039ce547afc60d0948eb5ae5b491ccd2c178dfb
Leman316 <56719469+Leman316@users.noreply.github.com>
2020-07-10 13:21:04 +0600

    new

--- new.R
+++ new.R
@@ -1,1 +1,55 @@
-print(22)
+
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
+###########################
+
+makeCacheMatrix <- function(x=matrix()){
+  inverse<-NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(i) inverse <<- i
+  getinverse <- function() inverse
+  list(set = set, get = get,setinverse = setinverse,getinverse = getinverse)
+  
+  
+}
+
+
+cachesolve <- function(x, ...) {
+  inverse <- x$getinverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  mat <- x$get()
+  inverse <- solve(mat, ...)
+  x$setinverse(mat)
+  inverse
+}
+
